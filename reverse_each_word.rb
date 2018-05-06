4075a7a4e4844d9bd1ff46516d386bbfcf820bb8
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-06 23:52:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
  array = string.split
 new_array = []
 array.each do |word|
-  word.reverse
-  new_array << word
+  new_word = word.reverse
+  new_array << new_word
   
 end
 new_array

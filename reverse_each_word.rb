07603b684fd11d9432843b65d99331934a0b8ab3
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-06 23:58:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,10 @@ def reverse_each_word(string)
  array = string.split
 
 array.collect do |word|
-  word.reverse
- 
-  
+  new_word = word.reverse
+  array << new_word
   
 end
-
+array.join(" ")
 end
 
